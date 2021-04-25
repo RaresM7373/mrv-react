94d8d610b3432d1481b86db60f658f850370ab7a
Rares Modure <m.rares956@yahoo.com>
2021-04-25 23:05:04 +0300

    Finished initial setup.

--- server/server.js
+++ server/server.js
@@ -5,10 +5,6 @@ const PORT = process.env.PORT || 8081;
 var app = express();
 app.use(bodyParser.json());
 
-app.get('/api/customers', (req, res) => {
-  res.status(200).send({success: true, customers: [{name: 'Rares'}, {name: 'Vlad'}]});
-});
-
 app.listen(PORT, () => {
   console.log('Server started new client.');
 });
