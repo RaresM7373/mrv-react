d27c3e71a251a53f50ae03138568185080a8cce7
Rares Modure <m.rares956@yahoo.com>
2021-04-25 21:40:53 +0300

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 8081;
+
+var app = express();
+app.use(bodyParser.json());
+
+app.get('/api/customers', (req, res) => {
+  res.status(200).send({success: true, customers: [{name: 'Rares'}, {name: 'Vlad'}]});
+});
+
+app.listen(PORT, () => {
+  console.log('Server started');
+});
